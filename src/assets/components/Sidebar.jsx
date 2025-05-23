ee37d0d3196b481b0a75a08a81e1ee14bce3e35e
Sara Calarese <sara120200@gmail.com>
2025-05-23 16:56:54 +0200

    aggiunta elementi sidebar

--- src/assets/components/Sidebar.jsx
+++ src/assets/components/Sidebar.jsx
@@ -10,6 +10,22 @@ function Sidebar() {
           +
         </button>
       </div>
+      <div className="container-rectangular">
+        <div className="rectangular">
+          <h3>Crea la tua prima playlist</h3>
+          <h4>E' facile, ti aiuteremo</h4>
+          <button type="button" className="btn-create">
+            Crea playlist
+          </button>
+        </div>
+        <div className="rectangular">
+          <h3>Cerca qualche podcast da seguire</h3>
+          <h4>Ti aggiorneremo sui nuovi episodi</h4>
+          <button type="button" className="btn-create">
+            Sfogia i podcast
+          </button>
+        </div>
+      </div>
     </aside>
   );
 }
